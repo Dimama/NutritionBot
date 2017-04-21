5cb140988d524e2b4b2cac098ae718689bb00d58
Dimama <citrus_m@mail.ru>
2017-04-21 14:20:43 +0300

    add parsing of test html-page

---
+++ parse.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+URL = 'http://snow-motion.ru/tablica-bzhu-kalorijnost'
+
+
+def get_html(url):
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, "lxml")
+
+    return soup.prettify()
+
+
+def parse_html(html):
+    soup = BeautifulSoup(html, "lxml")
+
+    products = []
+
+    for row in soup.table.find_all('tr')[1:]:
+        td = row.find_all('td')
+        products.append({
+                "name": td[0].text.strip(),
+                "protein": float(td[1].text.strip().replace(",", ".")),
+                "fat": float(td[2].text.strip().replace(",", ".")),
+                "carbohydrates": float(td[3].text.strip().replace(",", ".")),
+                "calories": float(td[5].text.strip().replace(",", ".")),
+            })
+
+    return products
+
+
+def print_table(products):
+    for pr in products:
+        print("{0} {1} {2} {3} {4}"
+              .format(pr["name"], pr["protein"], pr["fat"], pr["carbohydrates"], pr["calories"]))
+
+
+def main():
+    html = get_html(URL)
+    products = parse_html(html)
+    print_table(products)
+
+if __name__ == '__main__':
+    main()
