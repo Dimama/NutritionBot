644fa43603d4cfd1eb67bf5bec9677557cb88568
Dimama <citrus_m@mail.ru>
2017-05-28 00:09:03 +0300

    Added identify user's request from message

--- handler.py
+++ handler.py
@@ -1,6 +1,8 @@
 import dbhelper
 from config import mail
-
+import const
+import re
+import telebot
 
 class Handler(object):
     """ Класс для обработки сообщений пользователя"""
@@ -32,12 +34,38 @@ class Handler(object):
     def stat_by_period_info():
         """ Обработка команды 'Статистика за период' """
         answer = "Введите период в формате: \n" \
-                 "ДД.ММ.ГГГГ : ДД.ММ.ГГГГ"
+                 "ДД.ММ.ГГГГ-ДД.ММ.ГГГГ"
         return answer
 
     @staticmethod
     def set_update_data_info():
         """ Обработка команды 'Ввести/обновить личные данные' """
         answer = "Введите данные в формате: \n" \
-                 "(пол(м/ж), возраст, рост, вес)"
+                 "пол(м/ж), возраст, рост(см), вес(кг)"
         return answer
+
+
+    @staticmethod
+    def filter_request(message):
+        """ Определение запроса пользователя из текста сообщения """
+
+        if Handler.valid_text(const.reg_exp_dict["user data"], message.text):
+            answer = "True"
+        elif Handler.valid_text(const.reg_exp_dict["product"], message.text):
+            answer = "True"
+        elif Handler.valid_text(const.reg_exp_dict["day"], message.text):
+            answer = "True"
+        elif Handler.valid_text(const.reg_exp_dict["period"], message.text):
+            answer = "True"
+        else:
+            answer = "Я не знаю такой команды :("
+
+        return answer
+
+    @staticmethod
+    def valid_text(pattern, text):
+        res = re.match(pattern, text)
+        if res:
+            return True
+        return False
+
