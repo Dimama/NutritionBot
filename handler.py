6ec1c4d772a4fe88be78f126aae0fb5205b5f0f5
Dimama <citrus_m@mail.ru>
2017-05-28 14:41:06 +0300

    Regular exp. was changed. Added methods to filter data from user requests

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ import dbhelper
 from config import mail
 import const
 import re
-import telebot
+import datetime
 
 class Handler(object):
     """ Класс для обработки сообщений пользователя"""
@@ -50,13 +50,13 @@ class Handler(object):
         """ Определение запроса пользователя из текста сообщения """
 
         if Handler.valid_text(const.reg_exp_dict["user data"], message.text):
-            answer = "True"
+            answer = Handler.set_update_user_data(message)
         elif Handler.valid_text(const.reg_exp_dict["product"], message.text):
-            answer = "True"
+            answer = Handler.add_product(message)
         elif Handler.valid_text(const.reg_exp_dict["day"], message.text):
-            answer = "True"
+            answer = Handler.stat_day(message)
         elif Handler.valid_text(const.reg_exp_dict["period"], message.text):
-            answer = "True"
+            answer = Handler.stat_period(message)
         else:
             answer = "Я не знаю такой команды :("
 
@@ -69,3 +69,116 @@ class Handler(object):
             return True
         return False
 
+    @staticmethod
+    def set_update_user_data(message):
+        """ Метод для выделения данных пользователя из сообщения, их проверки и выполнения запроса к БД"""
+        data = message.text.strip().split(',')
+        try:
+            sex = data[0].lower()
+            age = int(data[1])
+            height = int(data[2])
+            weight = int(data[3])
+        except ValueError:
+            return const.error_emoji + " Ошибка обработки данных. Повторите ввод."
+
+        if age > const.MAX_AGE:
+            return const.error_emoji + " Введены некорректные данные.\n" \
+                                       "Максимальный возраст: {0}".format(const.MAX_AGE)
+
+        if height > const.MAX_HEIGHT:
+            return const.error_emoji + " Введены некорректные данные.\n" \
+                     "Максимальный рост: {0} см".format(const.MAX_HEIGHT)
+
+        if weight > const.MAX_WEIGHT:
+            return const.error_emoji + " Введены некорректные данные.\n" \
+                     "Максимальный вес: {0} кг".format(const.MAX_WEIGHT)
+
+        # Запрос к базе данных
+
+        answer = "Пол: {0}, Возраст: {1}, Рост: {2}, Вес: {3}".format(sex, age, height, weight)
+
+        return answer
+
+    @staticmethod
+    def add_product(message):
+        """ Метод для выделения данных о продукте, их проверки и выполнения запроса к БД"""
+        data = message.text.strip().split('-')
+
+        try:
+            product = data[0].replace(' ', '')
+            mass = int(data[1])
+        except ValueError:
+            return const.error_emoji + " Ошибка обработки данных. Повторите ввод."
+
+        if mass > const.MAX_PRODUCT_MASS:
+            return const.error_emoji + " Введены некорректные данные.\n" \
+                     "Максимальная масса продукта: {0} грамм".format(const.MAX_PRODUCT_MASS)
+
+        # Запрос к базе данных
+
+        answer = "Продукт: {0}, масса: {1}".format(product, mass)
+
+        return answer
+
+    @staticmethod
+    def is_correct_day(day, month, year):
+        if day < 1 or day > 31 or month < 1 or month > 12:
+            return False
+
+        try:
+            if datetime.date(year, month, day) > datetime.date.today():
+                return False
+        except ValueError:
+            return False
+
+        return True
+
+
+    @staticmethod
+    def stat_day(message):
+        """ Метод для выделения даты из сообщения, ее проверки и выполнения запроса к БД """
+
+        data = message.text.split('.')
+
+        try:
+            day = int(data[0])
+            month = int(data[1])
+            year = int(data[2])
+        except ValueError:
+            return const.error_emoji + " Ошибка обработки данных. Повторите ввод."
+
+        if not Handler.is_correct_day(day, month, year):
+            return const.error_emoji + " Введена некорректная дата."
+
+        # Запрос к базе данных
+
+        answer = "{0} - {1} - {2}".format(day, month, year)
+
+        return answer
+
+    @staticmethod
+    def stat_period(message):
+        """ Метод для выделения интервала дат из сообщения, его проверки и выполнения запроса к БД """
+
+        data = message.text.split('-')
+        date1, date2 = data[0].split('.'), data[1].split('.')
+
+
+        try:
+            day1, day2 = int(date1[0]), int(date2[0])
+            month1, month2 = int(date1[1]), int(date2[1])
+            year1, year2 = int(date1[2]), int(date2[2])
+        except ValueError:
+            return const.error_emoji + " Ошибка обработки данных. Повторите ввод."
+
+        if not Handler.is_correct_day(day1, month1, year1) or not Handler.is_correct_day(day2, month2, year2):
+            return const.error_emoji + " Введена некорректная дата."
+
+        if not(datetime.date(year1, month1, day1) < datetime.date(year2, month2, day2)):
+            return const.error_emoji + " Введен некорректный период."
+
+        # Запрос к базе данных
+
+        answer = "{0} - {1} - {2} :  {3} - {4} - {5}".format(day1, month1, year1, day2, month2, year2)
+
+        return answer
