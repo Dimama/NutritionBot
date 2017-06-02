4a2b4e9a0fb25fa41f2ea23f7c0a3d5d04685f87
Dimama <citrus_m@mail.ru>
2017-06-03 01:19:45 +0300

    Adding and updating personal data in DB added

--- dbhelper.py
+++ dbhelper.py
@@ -1,5 +1,5 @@
 import sqlite3
-from const import DB_NAME, FILE_TABLE
+from dbconfig import DB_NAME, FILE_TABLE
 import dbquery
 
 
@@ -14,12 +14,14 @@ class DBHelper(object):
             cursor = self.conn.cursor()
             products_data = DBHelper.load_table(FILE_TABLE)
             cursor.execute(dbquery.query_create_products_table)
-            cursor.execute(dbquery.query_select_size_products)
 
             # записываем продукты в таблицу, только если она только что создана
+            cursor.execute(dbquery.query_select_size_products)
             count_products = cursor.fetchone()[0]
             if count_products == 0:
                 cursor.executemany(dbquery.query_insert_product_into_products, products_data)
+
+            cursor.execute(dbquery.query_create_users_table)
 
         except sqlite3.DatabaseError as err:
             print("ERROR: ", err)
@@ -49,4 +51,28 @@ class DBHelper(object):
         pass
 
     def set_update_user_data(self, data):
-        pass
+        try:
+            cursor = self.conn.cursor()
+
+            # проверить наличие записи
+            cursor.execute(dbquery.query_select_user, (data["id"], ))
+            if cursor.fetchone():
+
+                cursor.execute(dbquery.query_update_user,
+                               (data["sex"], data["age"], data["height"], data["weight"], data["id"]))
+                answer = "Данные успешно обновлены."
+
+            else:
+                cursor.execute(dbquery.query_add_user, (data["id"], data["first_name"], data["second_name"],
+                                                        data["sex"], data["age"], data["height"], data["weight"]))
+                cursor.execute(dbquery.query_create_user_journal % ("journal_" + str(data["id"])))
+                answer = "Данные успешно добавлены."
+
+        except sqlite3.DatabaseError as err:
+            print("ERROR: ", err)
+            answer = "Возникла ошибка при добавлении данных. Повторите ввод."
+        else:
+            self.conn.commit()
+
+        self.conn.close()
+        return answer
--- dbquery.py
+++ dbquery.py
@@ -12,3 +12,21 @@ query_create_products_table = """CREATE TABLE IF NOT EXISTS
 query_select_size_products = """SELECT COUNT(*) FROM Products"""
 
 query_insert_product_into_products = """INSERT INTO Products Values(NULL, ?, ?, ?, ?, ?);"""
+
+
+query_create_users_table = """CREATE TABLE IF NOT EXISTS
+                                Users (
+                                    id INTEGER PRIMARY KEY NOT NULL,
+                                    first_name TEXT, last_name TEXT, sex TEXT, age INTEGER,
+                                    height INTEGER, weight INTEGER 
+                                    )"""
+
+query_add_user = """ INSERT INTO Users Values(?, ?, ?, ?, ?, ?, ?);"""
+
+query_update_user = """ UPDATE Users SET sex = ?, age = ?, height = ?, weight = ? WHERE id = ?"""
+
+query_select_user = """SELECT * FROM Users WHERE id = ?"""
+
+query_create_user_journal = """CREATE TABLE %s (datetime TEXT, product_id INTEGER,
+                                                 mass INTEGER, protein REAL, fat REAL, carbs REAL, calories REAL,
+                                                 FOREIGN KEY(product_id) REFERENCES Products(id))"""
