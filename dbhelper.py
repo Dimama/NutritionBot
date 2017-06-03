7f27249a59665c560a3548d87632cdd6de041cd0
Dimama <citrus_m@mail.ru>
2017-06-04 01:06:49 +0300

    Adding products to users journals in DB added

--- dbhelper.py
+++ dbhelper.py
@@ -1,6 +1,9 @@
 import sqlite3
 from dbconfig import DB_NAME, FILE_TABLE
 import dbquery
+from searchsimilar import SimilarSearcher
+from answerer import Answerer
+from calculator import Calculator
 
 
 class DBHelper(object):
@@ -13,15 +16,15 @@ class DBHelper(object):
         try:
             cursor = self.conn.cursor()
             products_data = DBHelper.load_table(FILE_TABLE)
-            cursor.execute(dbquery.query_create_products_table)
+            cursor.execute(dbquery.create_products_table)
 
             # записываем продукты в таблицу, только если она только что создана
-            cursor.execute(dbquery.query_select_size_products)
+            cursor.execute(dbquery.select_size_products)
             count_products = cursor.fetchone()[0]
             if count_products == 0:
-                cursor.executemany(dbquery.query_insert_product_into_products, products_data)
+                cursor.executemany(dbquery.insert_product_into_products, products_data)
 
-            cursor.execute(dbquery.query_create_users_table)
+            cursor.execute(dbquery.create_users_table)
 
         except sqlite3.DatabaseError as err:
             print("ERROR: ", err)
@@ -41,8 +44,58 @@ class DBHelper(object):
                 table.append((product[0], float(product[1]), float(product[2]), float(product[3]), float(product[4])))
         return table
 
-    def add_product(self, data):
-        pass
+    def add_product(self, data, by_id=False):
+        """ Добавление иформации о употребленном продукте в БД"""
+        try:
+            cursor = self.conn.cursor()
+
+            cursor.execute(dbquery.select_user, (data["id"],))
+            user = cursor.fetchone()
+            if user:
+                if by_id:
+                    cursor.execute(dbquery.select_product_info_by_id, (data["product_id"],))
+                    produсt = cursor.fetchone()
+                    if produсt:
+                        mass = data["mass"]
+                        protein, fat, carbs, calories = Calculator.calc_product_energy(mass, produсt[2],
+                                                                                produсt[3], produсt[4], produсt[5])
+                        cursor.execute(dbquery.insert_product_into_journal_by_id % str(data["id"]),
+                                       (data["product_id"], mass, protein, fat, carbs, calories))
+                        answer = "Запись добавлена."
+                    else:
+                        answer = "Продукт с таким номером не найден."
+
+                else:
+                    cursor.execute(dbquery.select_product_info_by_name, (data["product_name"],))
+                    produсt = cursor.fetchone()
+
+                    if produсt:
+                        mass = data["mass"]
+                        protein, fat, carbs, calories = Calculator.calc_product_energy(mass, produсt[2], produсt[3],
+                                                                                       produсt[4], produсt[5])
+                        cursor.execute(dbquery.insert_product_into_journal_by_name % str(data["id"]),
+                                       (data["product_name"], mass, protein, fat, carbs, calories))
+                        answer = "Запись добавлена."
+                    else:
+                        cursor.execute(dbquery.select_all_products)
+                        produсts = cursor.fetchall()
+                        searcher = SimilarSearcher(produсts)
+                        sim_prod = searcher.search_similar(data["product_name"])
+                        if sim_prod:
+                            answer = Answerer.similar_products(sim_prod)
+                        else:
+                            answer = Answerer.no_product()
+            else:
+                answer = "Введите персональные данные!"
+
+        except sqlite3.DatabaseError as err:
+            print("ERROR: ", err)
+            answer = "Возникла ошибка при добавлении данных. Повторите ввод."
+        else:
+            self.conn.commit()
+
+        self.conn.close()
+        return answer
 
     def get_stat_day(self, data):
         pass
@@ -51,21 +104,22 @@ class DBHelper(object):
         pass
 
     def set_update_user_data(self, data):
+        """ Обновление/ добавление данных пользователя в БД"""
         try:
             cursor = self.conn.cursor()
 
             # проверить наличие записи
-            cursor.execute(dbquery.query_select_user, (data["id"], ))
+            cursor.execute(dbquery.select_user, (data["id"],))
             if cursor.fetchone():
 
-                cursor.execute(dbquery.query_update_user,
+                cursor.execute(dbquery.update_user,
                                (data["sex"], data["age"], data["height"], data["weight"], data["id"]))
                 answer = "Данные успешно обновлены."
 
             else:
-                cursor.execute(dbquery.query_add_user, (data["id"], data["first_name"], data["second_name"],
-                                                        data["sex"], data["age"], data["height"], data["weight"]))
-                cursor.execute(dbquery.query_create_user_journal % ("journal_" + str(data["id"])))
+                cursor.execute(dbquery.add_user, (data["id"], data["first_name"], data["second_name"],
+                                                  data["sex"], data["age"], data["height"], data["weight"]))
+                cursor.execute(dbquery.create_user_journal % ("journal_" + str(data["id"])))
                 answer = "Данные успешно добавлены."
 
         except sqlite3.DatabaseError as err:
--- dbquery.py
+++ dbquery.py
@@ -1,6 +1,6 @@
 """ ЗАПРОСЫ К БД"""
 
-query_create_products_table = """CREATE TABLE IF NOT EXISTS 
+create_products_table = """CREATE TABLE IF NOT EXISTS 
                                     Products (
                                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                                         product TEXT,
@@ -9,24 +9,34 @@ query_create_products_table = """CREATE TABLE IF NOT EXISTS
                                         carbs REAL,
                                         calories REAL )"""
 
-query_select_size_products = """SELECT COUNT(*) FROM Products"""
+select_size_products = """SELECT COUNT(*) FROM Products"""
 
-query_insert_product_into_products = """INSERT INTO Products Values(NULL, ?, ?, ?, ?, ?);"""
+insert_product_into_products = """INSERT INTO Products Values(NULL, ?, ?, ?, ?, ?);"""
 
 
-query_create_users_table = """CREATE TABLE IF NOT EXISTS
+create_users_table = """CREATE TABLE IF NOT EXISTS
                                 Users (
                                     id INTEGER PRIMARY KEY NOT NULL,
                                     first_name TEXT, last_name TEXT, sex TEXT, age INTEGER,
                                     height INTEGER, weight INTEGER 
                                     )"""
 
-query_add_user = """ INSERT INTO Users Values(?, ?, ?, ?, ?, ?, ?);"""
+add_user = """ INSERT INTO Users Values(?, ?, ?, ?, ?, ?, ?);"""
 
-query_update_user = """ UPDATE Users SET sex = ?, age = ?, height = ?, weight = ? WHERE id = ?"""
+update_user = """ UPDATE Users SET sex = ?, age = ?, height = ?, weight = ? WHERE id = ?"""
 
-query_select_user = """SELECT * FROM Users WHERE id = ?"""
+select_user = """SELECT * FROM Users WHERE id = ?"""
 
-query_create_user_journal = """CREATE TABLE %s (datetime TEXT, product_id INTEGER,
+create_user_journal = """CREATE TABLE %s (datetime TEXT, product_id INTEGER,
                                                  mass INTEGER, protein REAL, fat REAL, carbs REAL, calories REAL,
                                                  FOREIGN KEY(product_id) REFERENCES Products(id))"""
+
+insert_product_into_journal_by_id = """INSERT INTO journal_%s Values(datetime('now', 'localtime'), ?, ?, ?, ?, ?, ?)"""
+insert_product_into_journal_by_name = """INSERT INTO journal_%s Values(datetime('now', 'localtime'),
+                                                    (SELECT id FROM Products WHERE product = ?), ?, ?, ?, ?, ?)"""
+
+select_product_info_by_name = """SELECT * FROM Products WHERE product = ?"""
+select_product_info_by_id = """SELECT * FROM Products WHERE id = ?"""
+select_all_products = """SELECT id, product FROM Products"""
+
+
